7c5a831909647ef55fe7d2c64c9e483daf6913dc
崔剑龙 <jianlongcui23@gmail.com>
2021-03-05 08:20:26 +0800

    :fire: file update

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn Java demo
+> The great thing about programming is that you can debug it when you don't know why
+
--- multi-thread/src/main/java/top/lstech/thread/lock/TwinsLock.java
+++ multi-thread/src/main/java/top/lstech/thread/lock/TwinsLock.java
@@ -1,10 +1,13 @@
 package top.lstech.thread.lock;
 
+import lombok.Data;
+
 /**
  * @Author: 义云
  * @Date: 2020/6/11 12:29 PM
  * @Version 1.0
  */
+@Data
 public class TwinsLock {
     /**
      * 设计一个工具，同一时刻，只允许至多两个线程同时访问，超过两个线程的访问将会被阻塞。
